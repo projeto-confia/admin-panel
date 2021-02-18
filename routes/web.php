6eed03d910723e6f27058bb03fdee208bd2b86ff
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-02-18 08:03:42 -0300

    Adjusts routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
-require __DIR__.'/web/auth.php';
+require __DIR__ . '/auth.php';
