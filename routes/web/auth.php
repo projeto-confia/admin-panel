17ded3375662062080781c05d76971e0b8a26493
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-02-18 08:05:20 -0300

    Removes unused files

--- routes/web/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-    ->middleware('guest')
-    ->name('login');
