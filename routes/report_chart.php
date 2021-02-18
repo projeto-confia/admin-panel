c747b495b06385ce5e04b8e1273508872e44a3df
Augusto Jose Moreira da Fonseca <ajosemf@gmail.com>
2021-02-18 18:11:40 -0300

    Adiciona NewsChartController

--- routes/report_chart.php
+++ routes/report_chart.php
@@ -1,10 +1,6 @@
 <?php
 
-// use App\Http\Controllers\Report\NewsController;
+use App\Http\Controllers\Report\NewsChartController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/report/news_chart', function(){
-    return view('pages.report.news_chart');
-} );
-
-// Route::get('/news_chart', [NewsChartController::class, 'index']);
+Route::get('/report/news_chart', [NewsChartController::class, 'index']);
