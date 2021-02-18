21b39a10c27dbe81ecd864be73475d7e4109e12e
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-02-18 09:24:11 -0300

    Adds startup script to create schemas

---
+++ docker/postgres/create-schemas.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$DB_USERNAME" --dbname "$DB_DATABASE" <<-EOSQL
+    CREATE SCHEMA IF NOT EXISTS detecenv AUTHORIZATION admin;
+    CREATE SCHEMA IF NOT EXISTS admin_panel AUTHORIZATION admin;
+EOSQL
