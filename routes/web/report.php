22ea525fcb7b790aa5763cefcfaab4d7eb096d1f
Augusto Jose Moreira da Fonseca <ajosemf@gmail.com>
2021-02-22 20:47:16 -0300

    Ajusta as rotas

--- routes/web/report.php
+++ routes/web/report.php
@@ -4,11 +4,7 @@ use App\Http\Controllers\Report\NewsController;
 use App\Http\Controllers\Report\NewsChartController;
 use Illuminate\Support\Facades\Route;
 
-// Route::prefix('report')->middleware('auth')->group(function () {
-//     Route::get('news', [NewsController::class, 'index'])->name('news.index');
-// });
-
-Route::prefix('report')->group(function () {
+Route::prefix('report')->middleware('auth')->group(function () {
     Route::get('news', [NewsController::class, 'index'])->name('news.index');
     Route::get('news_chart', [NewsChartController::class, 'index'])->name('news_chart.index');
 });
