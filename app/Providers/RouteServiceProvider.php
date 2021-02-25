eeface3be789864a1605739d3b5187eebe851856
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-02-24 21:38:48 -0300

    Changes default HOME route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/report/news';
 
     /**
      * The controller namespace for the application.
