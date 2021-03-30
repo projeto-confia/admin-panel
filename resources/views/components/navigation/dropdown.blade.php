10e40a08e8cc987e7280ef46418e5a1b7a10773e
Gabriel Machado <gabriel.rmachado10@gmail.com>
2021-03-30 13:50:53 -0300

    Pequenas alteraçoes no header.

--- resources/views/components/navigation/dropdown.blade.php
+++ resources/views/components/navigation/dropdown.blade.php
@@ -1,10 +1,10 @@
 @props(['id', 'label'])
 
 <li class="nav-item dropdown {{ $attributes->get('class') }}">
-    <a class="nav-link dropdown-toggle" href="#" id="{{ $id }}" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <a class="nav-link dropdown-toggle" href="#" id="{{ $id }}" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         {{ $label }}
     </a>
-    <ul class="dropdown-menu" aria-labelledby="{{ $id }}">
+    <ul class="dropdown-menu" style="position:fixed; left:auto; top:auto;" aria-labelledby="{{ $id }}">
         {{ $slot }}
     </ul>
 </li>
