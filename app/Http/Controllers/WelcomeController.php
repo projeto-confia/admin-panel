e4059226edf239ebf13eb5ca5a21346db3251ca7
Gabriel Machado <gabriel.rmachado10@gmail.com>
2021-04-04 22:07:16 -0300

    Caixa de pesquisa para o grafico dos top usuarios (ICS).

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -14,10 +14,12 @@ use App\Models\News;
 
 class WelcomeController extends Controller
 {
-    private function graph_top_users_ics(Request $request)
+    private function graph_top_users_fake_ics(Request $request)
     {
         $number_top_users = $request->inputTopUsers;
-        $data_top_users = DB::select('select * from detectenv.get_top_users_which_shared_most_fake_news_ics(?);', array($number_top_users));
+        $data_top_users = DB::select('select * from detectenv.get_top_users_which_shared_news_ics(?) order by total_fake_news desc, total_news asc;', array($number_top_users));
+
+        dd($data_top_users);
         
         # pega as chaves dos dados recuperados.
         $keys = array_keys(json_decode(json_encode($data_top_users[0]), true));
@@ -70,25 +72,11 @@ class WelcomeController extends Controller
     public function show(Request $request)
     {
         $loggedUser = auth()->user()->name;  
-        $json_top_users = $this->graph_top_users_ics($request);
+        $json_top_users = $this->graph_top_users_fake_ics($request);
         $total_news = $this->get_total_news();
 
         // dd($this->get_total_news_to_be_checked());
 
         return view('welcome', compact('loggedUser', 'json_top_users', 'total_news'));
     }
-
-    // select * from
-	// (select detectenv.social_media_account.id_account_social_media, detectenv.social_media_account.screen_name, 
-	//  count(detectenv.news.classification_outcome) as total_news,
-	//  count(detectenv.news.classification_outcome) filter (where detectenv.news.classification_outcome = true) as total_fake_news,
-	//  count(detectenv.news.classification_outcome) filter (where detectenv.news.classification_outcome = false) as total_not_fake_news
-	// from detectenv.post
-	// inner join detectenv.social_media_account
-	//  	on detectenv.post.id_social_media_account = detectenv.social_media_account.id_social_media_account
-	// inner join detectenv.news
-	//  	on detectenv.post.id_news = detectenv.news.id_news
-	// group by
-	// 	detectenv.social_media_account.id_account_social_media, detectenv.social_media_account.screen_name) tbl
-    // order by tbl.total_news desc
 }
