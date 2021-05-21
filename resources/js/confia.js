0f6c5c8f42e657687d5f1ad6b3dc031951a8b52e
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-05-21 18:59:04 -0300

    Move javascript para estrutura organizada

--- resources/js/confia.js
+++ resources/js/confia.js
@@ -1,11 +1,12 @@
 import { precisionNews } from "./pages/precisionNews";
 import { newsChart } from "./pages/newsChart";
-
+import { welcome } from "./pages/welcome";
 
 const CONFIA = {
     pages: {
         precisionNews,
         newsChart,
+        welcome,
     }
 };
 
