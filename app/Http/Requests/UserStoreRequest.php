f32009b9d5fea3f4a6aa117d9b339fdbed0335cb
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-07-17 21:23:55 -0300

    Ajusta regra de validação para criação de usuário

--- app/Http/Requests/UserStoreRequest.php
+++ app/Http/Requests/UserStoreRequest.php
@@ -25,7 +25,7 @@ class UserStoreRequest extends FormRequest
     {
         return [
             'name' => ['required', 'max:255'],
-            'email' => ['required', 'email'],
+            'email' => ['required', 'email', 'unique:users'],
             'is_admin' => ['sometimes', 'boolean'],
         ];
     }
