df6951537bb091ea0ed357887a0996a559598f30
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-07-19 21:23:32 -0300

    Corrige migration para aceitar null

--- database/migrations/2021_07_06_213046_adds_is_admin_column_in_user_table.php
+++ database/migrations/2021_07_06_213046_adds_is_admin_column_in_user_table.php
@@ -14,7 +14,7 @@ class AddsIsAdminColumnInUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_admin')->default(false)->nullable();
         });
     }
 
