09bf274152bc5dfcbed9532f0329056317c49884
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-08-18 19:33:58 -0300

    Cria model e controller de curadoria

---
+++ app/Models/Curatorship.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Curatorship extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'detectenv';
+    protected $table = 'curatorship';
+    protected $primaryKey = 'id_curatorship';
+    public $timestamps = false;
+
+    protected $casts = [
+        'is_news' => 'boolean',
+        'is_similar' => 'boolean',
+        'is_fake_news' => 'boolean',
+        'is_curated' => 'boolean',
+        'is_processed' => 'boolean',
+    ];
+
+    public function news(): BelongsTo
+    {
+        return $this->belongsTo(News::class, 'id_news', 'id_news');
+    }
+}
---
+++ routes/web/curatorship.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\CuratorshipController;
+use Illuminate\Support\Facades\Route;
+
+Route::resource('curadoria', CuratorshipController::class);
