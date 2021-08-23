e7cd665d8c79225541e4d7edbc3499c136b19591
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-08-23 20:57:21 -0300

    Cria métodos que realiza a curadoria em repository

--- app/Repositories/Automata/CuratorshipRepository.php
+++ app/Repositories/Automata/CuratorshipRepository.php
@@ -5,8 +5,10 @@ namespace App\Repositories\Automata;
 
 
 use App\Models\Automata\Curatorship;
+use App\Models\Automata\DTO\CurateDTO;
 use App\Models\Automata\DTO\CuratorshipDTO;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class CuratorshipRepository
 {
@@ -20,6 +22,40 @@ class CuratorshipRepository
             ->where('is_processed', false)
             ->orderBy('news.datetime_publication')
             ->paginate()
-            ->through(fn (Curatorship $curatorship) => new CuratorshipDTO($curatorship));
+            ->through(fn(Curatorship $curatorship) => new CuratorshipDTO($curatorship));
+    }
+
+    public function curate(Curatorship $curatorship, CurateDTO $curateDTO)
+    {
+        $curatorship->is_news = $curateDTO->isNews();
+        $curatorship->text_note = $curateDTO->getTextNote();
+        $curatorship->is_curated = true;
+
+        if ($curatorship->hasAgencyCheckedNews()) {
+            $curatorship->is_similar = $curateDTO->isSimilar();
+        }
+
+        if ($curateDTO->isNews()) {
+            $curatorship->is_fake = $curateDTO->isFake();
+            $curatorship->save();
+            return;
+        }
+
+        $curatorship->is_processed = true;
+        $curatorship->is_curated = true;
+        $curatorship->save();
+    }
+
+    /**
+     * Retrieve next curatorship register based on the reference
+     * @param int $idCuratorship
+     * @return CuratorshipDTO
+     * @throws ModelNotFoundException
+     */
+    public function getNextFrom(int $idCuratorship): CuratorshipDTO
+    {
+        /** @var Curatorship $curatorship*/
+        $curatorship = Curatorship::where('id',  '>', $idCuratorship)->firstOrFail();
+        return new CuratorshipDTO($curatorship);
     }
 }
