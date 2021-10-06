e97ea563e4e7c14d0c354d5acacec8864c9b1d58
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2021-10-05 22:09:02 -0300

    Revert "Ajustes em relatório de notícias"
    This reverts commit 041824ff15a2aa8a62fcc6d8acb554443ab4fa5e.

--- app/Models/Automata/News.php
+++ app/Models/Automata/News.php
@@ -6,7 +6,6 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
  * @property string text_news
@@ -55,10 +54,5 @@ class News extends Model
             'id_similarity_checking_outcome',
             'id_news',
         );
-    }
-
-    public function curatorship(): HasOne
-    {
-        return $this->hasOne(Curatorship::class, 'id_news', 'id_news');
     }
 }
