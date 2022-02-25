17711182415756c200d38c1ca94dd56b6dd3dad0
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2022-02-24 21:21:03 -0300

    Adiciona rotas

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ require __DIR__ . '/auth.php';
 require __DIR__.'/web/report.php';
 require __DIR__.'/web/users.php';
 require __DIR__.'/web/curatorship.php';
+require __DIR__.'/web/env_variable.php';
