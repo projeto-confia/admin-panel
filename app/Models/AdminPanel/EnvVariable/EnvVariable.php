9b0ce462f7134cd112aea7df529f3695d7129bff
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2022-03-10 23:16:11 -0300

    Mapeia atributos da entidade envEnvironment

--- app/Models/AdminPanel/EnvVariable/EnvVariable.php
+++ app/Models/AdminPanel/EnvVariable/EnvVariable.php
@@ -1,10 +1,17 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\AdminPanel\EnvVariable;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $value
+ * @property string $default_value
+ * @property string $type
+ * @property string $name
+ * @property string $description
+ */
 class EnvVariable extends Model
 {
     use HasFactory;
