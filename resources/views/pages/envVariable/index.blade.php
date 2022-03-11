7cf82d68acbc0c81f48264a49601314e4ee65d80
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2022-03-10 23:50:44 -0300

    Remove form desnecessário

--- resources/views/pages/envVariable/index.blade.php
+++ resources/views/pages/envVariable/index.blade.php
@@ -15,28 +15,23 @@
         <div class="d-flex flex-row-reverse mb-2">
             <a class="btn btn-primary" href="{{ route('usuarios.create') }}">Adicionar Usuário</a>
         </div>
-{{--            <form class="row g-3" action="{{route('configuration.update', $user->id)}}" method="POST" novalidate>--}}
-        <form class="row g-3" action="/" method="POST" novalidate>
-            @csrf
-            @method('PUT')
 
-            <ul class="list-group">
-                @foreach($envVariables as $envVariable)
-                <li class="list-group-item">
-                    <p class="h5">{{$envVariable->name}} @if(! $envVariable->updated)<span class="badge bg-warning">Não aplicada</span>@endif</p>
-                    <p>{{$envVariable->description}}</p>
-                    <p class="mb-0">Valor:</p>
-                    <textarea readonly class="m-0 w-100 bg-light rounded border-0" rows="1" >{{$envVariable->value}}</textarea>
+        <ul class="list-group">
+            @foreach($envVariables as $envVariable)
+            <li class="list-group-item">
+                <p class="h5">{{$envVariable->name}} @if(! $envVariable->updated)<span class="badge bg-warning">Não aplicada</span>@endif</p>
+                <p>{{$envVariable->description}}</p>
+                <p class="mb-0">Valor:</p>
+                <textarea readonly class="m-0 w-100 bg-light rounded border-0" rows="1" >{{$envVariable->value}}</textarea>
 
-                    <div class="btn-group float-end mt-2" role="group" aria-label="Basic example">
-                        <button type="button" class="btn btn-outline-danger">Apagar</button>
-                        <button type="button" class="btn btn-primary">Editar</button>
-                    </div>
-                </li>
-                @endforeach
-            </ul>
+                <div class="btn-group float-end mt-2" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-outline-danger">Apagar</button>
+                    <button type="button" class="btn btn-primary">Editar</button>
+                </div>
+            </li>
+            @endforeach
+        </ul>
 
-        </form>
     </main>
 
     @push('scripts')
