80eb294bcc1d7c49e438f98d5e4d87610d934e14
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2022-03-13 16:42:01 -0300

    Implementa lógica para recuperar listagem de templates para configurações

--- app/Http/Controllers/EnvVariableController.php
+++ app/Http/Controllers/EnvVariableController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\AdminPanel\EnvVariable\EnvVariable;
 use App\Services\Interfaces\IEnvVariableService;
-use App\View\Components\EnvVariable\EnvVariableComponent;
+use App\View\Components\EnvVariableType\EnvVariableComponentFactory;
+use App\View\Components\EnvVariableType\EnvVariableType;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
 
 class EnvVariableController extends Controller
 {
@@ -14,7 +15,25 @@ class EnvVariableController extends Controller
     {
     }
 
-    public function index(Request $request)
+    public function create(): View
+    {
+        $typesTemplate = array_map(
+            fn (string $type) => EnvVariableComponentFactory::create(new EnvVariableType($type, 'value', 'Valor')),
+            array_keys(EnvVariable::TYPES)
+        );
+
+        return view('pages.envVariable.create', [
+            'typesAvailable' => EnvVariable::TYPES,
+            'typesTemplate' => $typesTemplate
+        ]);
+    }
+
+    public function store(): View
+    {
+        return view('pages.envVariable.store');
+    }
+
+    public function index(): View
     {
         $envVariables = $this->envVariableService->all();
         $isUpdated = $this->envVariableService->isUpdated($envVariables);
