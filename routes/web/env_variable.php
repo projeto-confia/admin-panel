f8920d6fe6af129442d2356318cc59262c3e0440
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2022-03-13 16:37:30 -0300

    Cria Rota pra criar configurações

--- routes/web/env_variable.php
+++ routes/web/env_variable.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('configuracao/automata')->middleware('auth')->group(function () {
     Route::get('/', [EnvVariableController::class, 'index'])->name('configuration.index');
     Route::put('/', [EnvVariableController::class, 'update'])->name('configuration.update');
+    Route::get('/criar', [EnvVariableController::class, 'create'])->name('configuration.create');
+    Route::post('/criar', [EnvVariableController::class, 'store'])->name('configuration.store');
 });
