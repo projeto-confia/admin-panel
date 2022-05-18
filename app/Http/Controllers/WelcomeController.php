e0b06a705e1c19ca528b0c6ee246542f21085c19
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2022-05-18 20:55:11 -0300

    Remove consulta a procedure removida

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -13,8 +13,6 @@ class WelcomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $userCount = $request->get('user-count', 10);
-
         //  Feito consulta baseada em curadoria temporariamente
         $curatorshipCount = Curatorship::count();
 
@@ -23,9 +21,6 @@ class WelcomeController extends Controller
         return view(
             'welcome',
             [
-                'topFakeUsersJson' => $this->getTopFakeNewsSharingUsers($userCount),
-                'topNotFakeUsersJson' => $this->getTopNotFakeNewsSharingUsers($userCount),
-
                 'totalNewsPredicted' => News::whereNotNull('classification_outcome')
                     ->whereNotNull('prob_classification')
                     ->count(),
@@ -45,55 +40,4 @@ class WelcomeController extends Controller
             ]
         );
     }
-
-    /**
-     * Returns encoded json from top fake news sharing users
-     * @param int $userCount
-     * @return string
-     */
-    private function getTopFakeNewsSharingUsers(int $userCount = 10): string
-    {
-        $query = 'select * from detectenv.get_top_users_which_shared_news_ics() order by rate_fake_news desc limit ?;';
-        $userDataResult = DB::select($query, [$userCount]);
-
-        return $this->groupResultByKey($userDataResult)->toJson();
-    }
-
-    /**
-     * Returns encoded json from top NOT fake news sharing users
-     * @param int $userCount
-     * @return string
-     */
-    private function getTopNotFakeNewsSharingUsers(int $userCount = 10): string
-    {
-        $query = 'select * from detectenv.get_top_users_which_shared_news_ics() order by rate_not_fake_news desc limit ?;';
-        $userDataResult = DB::select($query, [$userCount]);
-
-        return $this->groupResultByKey($userDataResult)->toJson();
-    }
-
-    private function groupResultByKey(array $result): Collection
-    {
-        $resultCollection = collect($result);
-        $keys = array_keys((array) $resultCollection->first());
-
-        $initialValue = collect(array_reduce(
-            $keys,
-            function ($acc, $key) {
-                $acc[$key] = [];
-                return $acc;
-            },
-            []
-        ));
-
-        $reducer = fn($accumulator, \stdClass $item) => $accumulator
-            ->map(function ($value, string $key) use ($item) {
-                $itemArray = (array) $item;
-                array_push($value, $itemArray[$key]);
-                return $value;
-            });
-
-        return $resultCollection->reduce($reducer, $initialValue);
-    }
-
 }
