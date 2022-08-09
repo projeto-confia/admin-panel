6f92f34f1a28b4a97794bfe5e394f7c67f2cf612
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2022-08-08 21:07:08 -0300

    Reinicia estado dos inputs radios da curadoria

--- resources/js/pages/curatorship/curate.js
+++ resources/js/pages/curatorship/curate.js
@@ -1,12 +1,16 @@
 const curate = () => {
     const isNewsRadios = document.querySelectorAll('[name="is_news"]');
     const isSimilarRadios = document.querySelectorAll('[name="is_similar"]');
-    const isFakeNewsFieldWrapper = document.querySelector('#is_fake_news_field_wrapper');
+    const isFakeRadios = document.querySelectorAll('[name="is_fake_news"]');
     const isNewsFieldWrapper = document.querySelector('#is_news_field_wrapper');
+    const isFakeNewsFieldWrapper = document.querySelector('#is_fake_news_field_wrapper');
+
+    const resetRadiosInputs = inputs => Array.from(inputs).forEach(input => input.checked = false);
 
     Array.from(isNewsRadios).forEach(isNewsRadioOption =>
         isNewsRadioOption
             .addEventListener('change', ({ target: { value: isNews } }) => {
+                resetRadiosInputs(isFakeRadios);
                 if (+isNews) {
                     isFakeNewsFieldWrapper.classList.remove('d-none');
                     return;
@@ -18,10 +22,16 @@ const curate = () => {
 
     Array.from(isSimilarRadios).forEach(isSimilarRadioOption =>
         isSimilarRadioOption.addEventListener('change', ({ target: { value: isSimilar } }) => {
+            resetRadiosInputs(isNewsRadios);
+            resetRadiosInputs(isFakeRadios);
+            isFakeNewsFieldWrapper.classList.add('d-none');
+
             if (+isSimilar) {
                 isNewsFieldWrapper.classList.add('d-none');
                 return;
             }
+            resetRadiosInputs(isNewsRadios);
+            resetRadiosInputs(isFakeRadios);
             isNewsFieldWrapper.classList.remove('d-none');
         })
     );
